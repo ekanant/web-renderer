ee62f41de9e43f7db4ed890542fc6a4477d462b2
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2021-08-04 00:10:54 +0700

    Render pdf by puppeteer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
-const express = require('express')
+const express = require('express');
+const puppeteer = require('puppeteer');
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    const { url = "" } = req.query;
-    res.send('Hello World!')
+app.get('/web-renderer/pdf', async (req, res) => {
+    const { url = "", format="A4" } = req.query;
+
+    if(url) {
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+        await page.goto(url, {waitUntil: 'networkidle0'});
+        const pdf = await page.pdf({ format, displayHeaderFooter: false, printBackground: true });
+
+        console.log("pdf", pdf);
+    
+        await browser.close();
+        res.send(pdf)
+    } else {
+        res.send('Hello World!url='+url)
+    }
 })
 
 app.listen(port, () => {
