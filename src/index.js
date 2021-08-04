bb093ca732867c1840214e14dab9cf34af6b33c9
Ekanant Phanuwatyingyong <ekanant2535@gmail.com>
2021-08-04 10:21:06 +0700

    Create printPdf function

--- src/index.js
+++ src/index.js
@@ -3,19 +3,31 @@ const puppeteer = require('puppeteer');
 const app = express()
 const port = 3000
 
+const printPDF = async ({url = "", format="A4", waitUntil = "load"}) => {
+    const browser = await puppeteer.launch({ headless: true, args: ['--disable-dev-shm-usage']});   //--disable-dev-shm-usage for run on docker
+    const page = await browser.newPage();
+    await page.goto(url, {waitUntil});
+    const pdf = await page.pdf({ format, displayHeaderFooter: false, printBackground: true });
+   
+    await browser.close();
+    return pdf
+  }
+
 app.get('/web-renderer/pdf', async (req, res) => {
-    const { url = "", format="A4" } = req.query;
+    const { url = "", format="A4", waitUntil = "load" } = req.query;
 
     if(url) {
-        const browser = await puppeteer.launch({ headless: true });
-        const page = await browser.newPage();
-        await page.goto(url, {waitUntil: 'networkidle0'});
-        const pdf = await page.pdf({ format, displayHeaderFooter: false, printBackground: true });
-
-        console.log("pdf", pdf);
-    
-        await browser.close();
-        res.send(pdf)
+        try {
+            const pdf = await printPDF({
+                url, format, waitUntil
+            })
+            res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+            res.send(pdf)
+        } catch (err) {
+            console.error(`error url=${url}`, err)
+            res.send("Error");
+        }
+        
     } else {
         res.send('Hello World!url='+url)
     }
